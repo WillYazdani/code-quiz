0347916340abb18ab6306af79fdc64cc8d90bc26
Will Yazdani <yazd0006@gmail.com>
2022-01-23 21:39:18 -0600

    startgame function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,33 @@
 var timeEl = document.getElementById("timer")
 var timeRemainder = 75;
+var i = 0;
 
 //btn
 var btnEl = document.querySelector("#btn");
 btnEl.addEventListener("click", function() {
     timer()
+    gameStart()
 });
 
 //timer
-function timer () {
+var timer = function() {
     var timeInt = setInterval (function() {
-        timeEl.textContent = "Time:" + timeRemainder + " seconds"
+        timeEl.textContent = "Time: " + timeRemainder
         timeRemainder--;
 
-        if (timeRemainder===0) {
+        if (timeRemainder<=0) {
             timeEl.textContent = "Time's Up! GAME OVER!";
             clearInterval(timeInt);
         }
     }, 1000);
+};
+
+var questions = [
+    {
+        questionText: "this will be a question",
+    }
+]
+
+var gameStart = function() {
+    document.getElementById("content").innerText = questions[i].questionText
 };
