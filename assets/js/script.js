0e423a45be169e6707a46b287381da0ad8daaa0f
Will Yazdani <yazd0006@gmail.com>
2022-01-23 22:06:20 -0600

    added question to button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var timeRemainder = 75;
 var i = 0;
 
 //btn
-var btnEl = document.querySelector("#btn");
+var btnEl = document.querySelector("#start-btn");
 btnEl.addEventListener("click", function() {
     timer()
     gameStart()
@@ -22,12 +22,48 @@ var timer = function() {
     }, 1000);
 };
 
+//questions
 var questions = [
     {
-        questionText: "this will be a question",
+        questionText: "This will be a question",
+        choice: ["1","2","3","4"]
     }
 ]
 
+//game function
 var gameStart = function() {
-    document.getElementById("content").innerText = questions[i].questionText
+
+    //remove welcome & btn
+    document.getElementById("start-btn").remove()
+    document.getElementById("welcome").remove()
+
+    //question
+    document.getElementById("question").innerText = questions[i].questionText
+
+    //answer btns
+    document.getElementById("choice").appendChild
+
+    var btn0 = document.createElement("button");
+    document.getElementById("choice").appendChild(btn0);
+    btn0.textContent = questions[i].choice[0];
+    btn0.id = "btn0"
+    btn0.className = "btn";
+
+    var btn1 = document.createElement("button");
+    document.getElementById("choice").appendChild(btn1);
+    btn1.textContent = questions[i].choice[1];
+    btn1.id = "btn1"
+    btn1.className = "btn";
+
+    var btn2 = document.createElement("button");
+    document.getElementById("choice").appendChild(btn2);
+    btn2.textContent = questions[i].choice[2];
+    btn2.id = "btn2"
+    btn2.className = "btn";
+
+    var btn3 = document.createElement("button");
+    document.getElementById("choice").appendChild(btn3);
+    btn3.textContent = questions[i].choice[3];
+    btn2.id = "btn2"
+    btn3.className = "btn";
 };
